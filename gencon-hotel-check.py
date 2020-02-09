8143aa5a601f345e6ac32664aa58a1c927583f8b
Kalvin Krueger <kalvin727@gmail.com>
2020-02-09 15:13:06 -0600

    Slim down features

--- gencon-hotel-check.py
+++ gencon-hotel-check.py
@@ -9,9 +9,11 @@ from ssl import create_default_context as create_ssl_context, CERT_NONE, SSLErro
 from sys import stdout, version_info
 from threading import Thread
 from time import sleep
+from pushbullet import Pushbullet
+import requests
 
 if version_info < (2, 7, 9):
-	print("Requires Python 2.7.9+")
+    	print("Requires Python 2.7.9+")
 	exit(1)
 elif version_info.major == 2:
 	from HTMLParser import HTMLParser
@@ -23,9 +25,13 @@ else:
 	from urllib.parse import urlencode
 	from urllib.request import HTTPCookieProcessor, Request, urlopen, build_opener
 
-firstDay, lastDay, startDay = datetime(2019, 7, 27), datetime(2019, 8, 6), datetime(2019, 8, 1)
-eventId = 49822766
+firstDay, lastDay, startDay = datetime(2020, 7, 29), datetime(2020, 8, 2), datetime(2020, 7, 30)
+
+
+eventId = 50023680
 ownerId = 10909638
+
+targetUrl = ''
 
 distanceUnits = {
 	1: 'blocks',
@@ -82,32 +88,19 @@ def type_regex(arg):
 		raise ArgumentTypeError("invalid regex '%s': %s" % (arg, e))
 
 class PasskeyUrlAction(Action):
-	def __call__(self, parser, namespace, values, option_string = None):
+    def __call__(self, parser, namespace, values, option_string = None):
 		m = reCompile('^https://book.passkey.com/reg/([0-9A-Z]{8}-[0-9A-Z]{4})/([0-9a-f]{1,64})$').match(values)
 		if m:
 			setattr(namespace, self.dest, m.groups())
 		else:
 			raise ArgumentError(self, "invalid passkey url: '%s'" % values)
 
-class SurnameAction(Action):
-	def __call__(self, parser, namespace, values, option_string = None):
-		raise ArgumentError(self, "option no longer exists. Surname should be passed along with the key")
-
-class EmailAction(Action):
-	def __call__(self, parser, namespace, values, option_string = None):
-		dest = getattr(namespace, self.dest)
-		if dest is None:
-			dest = []
-			setattr(namespace, self.dest, dest)
-		dest.append(tuple(['email'] + values))
-
 parser = ArgumentParser()
-parser.add_argument('--surname', '--lastname', action = SurnameAction, help = SUPPRESS)
 parser.add_argument('--guests', type = int, default = 1, help = 'number of guests')
 parser.add_argument('--children', type = int, default = 0, help = 'number of children')
 parser.add_argument('--rooms', type = int, default = 1, help = 'number of rooms')
 group = parser.add_mutually_exclusive_group()
-group.add_argument('--checkin', type = type_day, metavar = 'YYYY-MM-DD', default = startDay.strftime('%Y-%m-%d'), help = 'check in')
+group.add_argument('--checkin', type = type_day, metavar = 'YYYY-MM-DD', default = (startDay - timedelta(1)).strftime('%Y-%m-%d'), help = 'check in')
 group.add_argument('--wednesday', dest = 'checkin', action = 'store_const', const = (startDay - timedelta(1)).strftime('%Y-%m-%d'), help = 'check in on Wednesday')
 parser.add_argument('--checkout', type = type_day, metavar = 'YYYY-MM-DD', default = (startDay + timedelta(3)).strftime('%Y-%m-%d'), help = 'check out')
 group = parser.add_mutually_exclusive_group()
@@ -127,101 +120,37 @@ group = parser.add_argument_group('required arguments')
 group.add_argument('--key', nargs = 2, metavar = ('KEY', 'AUTH'), help = 'key (see the README for more information)')
 group.add_argument('--url', action = PasskeyUrlAction, dest = 'key', help = 'passkey URL containing your key')
 
-group = parser.add_argument_group('alerts')
-group.add_argument('--popup', dest = 'alerts', action = 'append_const', const = ('popup',), help = 'show a dialog box')
-group.add_argument('--cmd', dest = 'alerts', action = 'append', type = lambda arg: ('cmd', arg), metavar = 'CMD', help = 'run the specified command, passing each hotel name as an argument')
-group.add_argument('--browser', dest = 'alerts', action = 'append_const', const = ('browser',), help = 'open the Passkey website in the default browser')
-group.add_argument('--email', dest = 'alerts', action = EmailAction, nargs = 3, metavar = ('HOST', 'FROM', 'TO'), help = 'send an e-mail')
-
 args = parser.parse_args()
-
-# Attempt to check the version against Github, but ignore it if it fails
-# Only updating the version when a breaking bug is fixed (a crash or a failure to search correctly)
-try:
-	version = open(pathjoin(dirname(abspath(__file__)), 'version')).read()
-	resp = urlopen('https://raw.githubusercontent.com/mrozekma/gencon-hotel-check/master/version')
-	if resp.getcode() == 200:
-		head = resp.read().decode('utf8')
-		if version != head:
-			print("Warning: This script is out-of-date. If you downloaded it via git, use 'git pull' to fetch the latest version. Otherwise, visit https://github.com/mrozekma/gencon-hotel-check")
-			print()
-except (HTTPError, IOError):
-	pass
 
 baseUrl = "https://book.passkey.com/event/%d/owner/%d" % (eventId, ownerId)
 
-# Setup the alert handlers
-alertFns = []
-success = True
-for alert in args.alerts or []:
-	if alert[0] == 'popup':
-		try:
-			import win32api
-			alertFns.append(lambda preamble, hotels: win32api.MessageBox(0, 'Gencon Hotel Search', "%s\n\n%s" % (preamble, '\n'.join("%s: %s: %s" % (hotel['distance'], hotel['name'], hotel['room']) for hotel in hotels))))
-		except ImportError:
-			try:
-				import tkinter, tkinter.messagebox
-				def handle(preamble, hotels):
-					window = tkinter.Tk()
-					window.wm_withdraw()
-					tkinter.messagebox.showinfo(title = 'Gencon Hotel Search', message = "%s\n\n%s" % (preamble, '\n'.join("%s: %s: %s" % (hotel['distance'], hotel['name'], hotel['room']) for hotel in hotels)))
-					window.destroy()
-				alertFns.append(handle)
-			except ImportError:
-				print("Unable to show a popup. Install either win32api (if on Windows) or Tkinter")
-				success = False
-	elif alert[0] == 'cmd':
-		import subprocess
-		alertFns.append(lambda preamble, hotels, cmd = alert[1]: subprocess.Popen([cmd] + ["%s: %s" % (hotel['name'], hotel['room']) for hotel in hotels]))
-	elif alert[0] == 'browser':
-		import webbrowser
-		alertFns.append(lambda preamble, hotels: webbrowser.open(baseUrl + '/home'))
-	elif alert[0] == 'email':
-		from email.mime.text import MIMEText
-		import getpass, smtplib, socket
-		_, host, fromEmail, toEmail = alert
-		password = getpass.getpass("Enter password for %s (or blank if %s requires no authentication): " % (fromEmail, host))
-		def smtpConnect():
-			try:
-				smtp = smtplib.SMTP_SSL(host)
-			except socket.error:
-				smtp = smtplib.SMTP(host)
-			if password:
-				smtp.login(fromEmail, password)
-			return smtp
-		try:
-			smtpConnect()
-			def handle(preamble, hotels):
-				msg = MIMEText("%s\n\n%s\n\n%s" % (preamble, '\n'.join("  * %s: %s: %s" % (hotel['distance'], hotel['name'].encode('utf-8'), hotel['room'].encode('utf-8')) for hotel in hotels), baseUrl + '/home'), 'plain', 'utf-8')
-				msg['Subject'] = 'Gencon Hotel Search'
-				msg['From'] = fromEmail
-				msg['To'] = toEmail
-				smtpConnect().sendmail(fromEmail, toEmail, msg.as_string())
-			alertFns.append(handle)
-		except Exception as e:
-			print(e)
-			success = False
+def notifyPushbullet():
+    	pushbulletKey = ''
+    	pb = Pushbullet(pushbulletKey)
+	pb.push_link("Gencon Housing Notification", targetUrl)
 
-if not success:
-	exit(1)
-if not alertFns:
-	print("Warning: You have no alert methods selected, so you're not going to know about a match unless you're staring at this window when it happens. See the README for more information")
-	print()
+def notifyDiscord():
+    discordWebhookUrl = ''
+	message = "Gencon Downton Hotel Notification: "+ targetUrl
+	data = {"content":message}
+	requests.post(discordWebhookUrl, data)
 
 if args.test:
 	print("Testing alerts one at a time...")
-	preamble = 'This is a test'
-	hotels = [{'name': 'Test hotel 1', 'distance': '2 blocks', 'rooms': 1, 'room': 'Queen/Queen suite'}, {'name': 'Test hotel 2', 'distance': '5 blocks', 'rooms': 5, 'room': 'Standard King'}]
-	for fn in alertFns:
-		fn(preamble, hotels)
-	print("Done")
-	exit(0)
 
-lastAlerts = set()
+	print("Testing Pushbullet...")
+	notifyPushbullet()
+
+	print("Testing Discord...")
+	notifyDiscord()
+	
+	print("Done")
+ 	exit(0)
+
 opener = build_opener(HTTPCookieProcessor())
 
 def send(name, *args):
-	try:
+   	try:
 		resp = opener.open(*args)
 		if resp.getcode() != 200:
 			raise RuntimeError("%s failed: %d" % (name, resp.getcode()))
@@ -306,37 +235,29 @@ def parseResults():
 			if simpleHotel['rooms'] == 0:
 				continue
 			result = "%-15s $%-9s %-80s (%d) %s" % (simpleHotel['distance'], simpleHotel['price'], simpleHotel['name'], simpleHotel['rooms'], simpleHotel['room'])
+
+			#nameMatch = ('Fair' in simpleHotel['name']) or ('fair' in simpleHotel['name']) or ('Spring' in simpleHotel['name']) or ('spring' in simpleHotel['name']) or ('Court' in simpleHotel['name']) or ('court' in simpleHotel['name']) or ('Embassy' in simpleHotel['name']) or ('embassy' in simpleHotel['name'])
+
+			#connected = True
+
 			# I don't think these distances (yards, meters, kilometers) actually appear in the results, but if they do assume it must be close enough regardless of --max-distance
-			closeEnough = hotel['distanceUnit'] in (2, 4, 5) or \
-			              (hotel['distanceUnit'] == 1 and (args.max_distance is None or (isinstance(args.max_distance, float) and hotel['distanceFromEvent'] <= args.max_distance))) or \
-			              (args.max_distance == 'connected' and connected)
-			cheapEnough = simpleHotel['price'] <= args.budget
-			regexMatch = args.hotel_regex.search(simpleHotel['name']) and args.room_regex.search(simpleHotel['room'])
-			if closeEnough and cheapEnough and regexMatch:
+			closeEnough = connected \
+			#Uncomment for blocks
+			#or hotel['distanceUnit'] == 1 or hotel['distanceFromEvent'] == 1 or hotel['distanceUnit'] in (2, 4, 5) or \
+			#(hotel['distanceUnit'] == 1 and (args.max_distance is None or (isinstance(args.max_distance, float) and hotel['distanceFromEvent'] <= args.max_distance))) or \
+			(args.max_distance == 'connected' and connected)
+			#if closeEnough and nameMatch:
+			if closeEnough:
 				alerts.append(simpleHotel)
 				stdout.write(' ! ')
 			else:
 				stdout.write('   ')
 			print(result)
 
-	global lastAlerts
 	if alerts:
-		alertHash = {(alert['name'], alert['room']) for alert in alerts}
-		if alertHash <= lastAlerts:
-			print("Skipped alerts (no new rooms in nearby hotel list)")
-		else:
-			numHotels = len(set(alert['name'] for alert in alerts))
-			preamble = "%d %s near the ICC:" % (numHotels, 'hotel' if numHotels == 1 else 'hotels')
-			for fn in alertFns:
-				# Run each alert on its own thread since some (e.g. popups) are blocking and some (e.g. e-mail) can throw
-				Thread(target = fn, args = (preamble, alerts)).start()
-			print("Triggered alerts")
-	else:
-		alertHash = set()
-
-	print()
-	lastAlerts = alertHash
-	return True
+    		notifyPushbullet()
+		notifyDiscord()
+		return True
 
 search = searchNew if '-' in args.key[0] else searchExisting
 while True:
